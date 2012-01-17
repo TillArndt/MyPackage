06abbde47986f6a7bfa7bd007f7cf6f7d7e1d2dc
htholen <>
2012-01-17 10:16:12 +0000

    added old file test/makeEnrichedSample_cfg.py + random commit

--- TtGammaAnalysis/src/PATPhotonHistoAnalyser.cc
+++ TtGammaAnalysis/src/PATPhotonHistoAnalyser.cc
@@ -8,9 +8,10 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "CommonTools/UtilAlgos/interface/HistoAnalyzer.h"
 #include "DataFormats/PatCandidates/interface/Photon.h"
-#include "DataFormats/Common/interface/View.h"
+//#include "DataFormats/Common/interface/View.h"
+#include <vector>
 
-typedef HistoAnalyzer<edm::View<pat::Photon> > PATPhotonHistoAnalyzer;
+typedef HistoAnalyzer<std::vector<pat::Photon> > PATPhotonHistoAnalyzer;
 
 DEFINE_FWK_MODULE( PATPhotonHistoAnalyzer );
 
--- TtGammaAnalysis/test/myPhotonSelection_cfg.py
+++ TtGammaAnalysis/test/myPhotonSelection_cfg.py
@@ -9,7 +9,7 @@ process.source = cms.Source("PoolSource",
 )
 
 process.TFileService = cms.Service("TFileService",
-  fileName = cms.string('output/myttbarSelection.root')
+  fileName = cms.string('output/myPhotonSelection.root')
 )
 
 import FWCore.MessageService.MessageLogger_cfi as logger
@@ -17,33 +17,96 @@ logger.MessageLogger.cerr.FwkReport.reportEvery = 100
 logger.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.extend(logger)
 
+#max num of events processed
+#process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
+
 #################################################################################
 ## Selection of events with a hard photon               
 
-process.myLargePtPhotons = cms.EDFilter("PATPhotonRefSelector",
-                                        src = cms.InputTag("patPhotonsPFlow"),
-                                        cut = cms.string('\
+# large pt ...
+process.myLargePtPhotons = cms.EDFilter("PATPhotonSelector",
+            src = cms.InputTag("patPhotonsPFlow"),
+            cut = cms.string('\
     pt > 15 \
     && abs(eta) < 2.1 \
-                                                          '),
-                                        filter = cms.bool(False)
-                                        )
+                              '),
+            filter = cms.bool(False)
+            )
 process.myLargePtPhotonCounter = cms.EDFilter("PATCandViewCountFilter",
-                                              src = cms.InputTag("myLargePtPhotons"),
-                                              minNumber = cms.uint32(1),
-                                              maxNumber = cms.uint32(9999)
-                                              )
+            src = cms.InputTag("myLargePtPhotons"),
+            minNumber = cms.uint32(1),
+            maxNumber = cms.uint32(9999)
+            )
 
-process.myPhotonAnalyser = cms.EDAnalyzer(
+# cross object clean against jets
+process.cocPatPhotons = cms.EDProducer("PATPhotonCleaner",
+            src = cms.InputTag("myLargePtPhotons"), 
+            preselection = cms.string(''),
+            
+            # overlap checking configurables
+            checkOverlaps = cms.PSet(
+                jets = cms.PSet(
+                   src       = cms.InputTag("patJetsPFlow"),
+                   algorithm = cms.string("byDeltaR"),
+                   preselection        = cms.string(""),  # don't preselect the muons
+                   deltaR              = cms.double(0.2),
+                   checkRecoComponents = cms.bool(False), # don't check if they share some AOD object ref
+                   pairCut             = cms.string(""),
+                   requireNoOverlaps   = cms.bool(True),
+                ),
+            ),
+            finalCut = cms.string(''),
+)
+
+#dummy to remove coc fails
+process.removeCocFails = cms.EDFilter("PATCandViewCountFilter",
+            src = cms.InputTag("cocPatPhotons"),
+            minNumber = cms.uint32(1),
+            maxNumber = cms.uint32(9999)
+            )
+
+process.photonsWithTightID = cms.EDFilter("PATPhotonSelector",
+            src = cms.InputTag("cocPatPhotons"),
+            cut = cms.string('photonID("PhotonCutBasedIDTight")'),
+            filter = cms.bool(True)
+            )
+
+# info's from generated Particles 
+process.photonsWithGenPart  = cms.EDFilter("PATPhotonSelector",
+            src = cms.InputTag("cocPatPhotons"),
+            cut = cms.string('genParticlesSize > 0'),
+            filter = cms.bool(True)
+            )
+
+
+
+#################################################################################
+## Some analyzing
+
+process.PATPhotonAnalyzer = cms.EDAnalyzer(
     "PATPhotonHistoAnalyzer",
-    src = cms.InputTag("myLargePtPhotons"),
+    src = cms.InputTag("photonsWithGenPart"),
     histograms = cms.VPSet(
-            cms.PSet(min          = cms.untracked.double(     -50.5),
-                     max          = cms.untracked.double(      50.5),
-                     nbins        = cms.untracked.int32 (       101),
+            cms.PSet(min          = cms.untracked.double(     -250.5),
+                     max          = cms.untracked.double(      250.5),
+                     nbins        = cms.untracked.int32 (       501),
                      name         = cms.untracked.string('motherId'),
                      description  = cms.untracked.string(        ''),
                      plotquantity = cms.untracked.string('genParticle.mother.pdgId')
+                     ),        
+            cms.PSet(min          = cms.untracked.double(        0.),
+                     max          = cms.untracked.double(       500),
+                     nbins        = cms.untracked.int32 (       250),
+                     name         = cms.untracked.string('genPart_Pt'),
+                     description  = cms.untracked.string(        ''),
+                     plotquantity = cms.untracked.string('genParticle.pt')
+                     ),        
+            cms.PSet(min          = cms.untracked.double(        0.),
+                     max          = cms.untracked.double(       500),
+                     nbins        = cms.untracked.int32 (       250),
+                     name         = cms.untracked.string('Pt'),
+                     description  = cms.untracked.string(        ''),
+                     plotquantity = cms.untracked.string('pt')
                      )        
         )
 )
@@ -55,5 +118,9 @@ process.myPhotonAnalyser = cms.EDAnalyzer(
 
 process.p = cms.Path(process.myLargePtPhotons *
                      process.myLargePtPhotonCounter *
-                     process.myPhotonAnalyser
+                     process.cocPatPhotons *
+                     process.removeCocFails *
+                     #process.photonsWithTightID *
+                     process.photonsWithGenPart *
+                     process.PATPhotonAnalyzer
                      )
