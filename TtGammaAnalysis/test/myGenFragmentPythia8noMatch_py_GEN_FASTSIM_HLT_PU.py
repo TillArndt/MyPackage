427264567847ef49ace30ecfde2b05755049d5f6
htholen <>
2012-05-22 13:00:22 +0000

    gen fragment with pythia 8

---
+++ TtGammaAnalysis/test/myGenFragmentPythia8noMatch_py_GEN_FASTSIM_HLT_PU.py
@@ -0,0 +1,83 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.341.2.2 
+# Source: /local/reps/CMSSW.admin/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: MyPackage/TtGammaAnalysis/python/myGenFragmentPythia8noMatch.py -s GEN,FASTSIM,HLT:GRun --conditions=FrontierConditions_GlobalTag,START44_V10::All --pileup=E7TeV_Fall2011_Reprocess_inTimeOnly --eventcontent=RECOSIM --filetype=LHE --filein=file:/net/data_cms/institut_3b/tholen/lhef/tt_nodecay/tt_nodecay_0.lhe --fileout=file:/netdata_cms/institut_3b/tholen/test/tt_nodecay.root -n 10000 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('FastSimulation.Configuration.EventContent_cff')
+process.load('FastSimulation.PileUpProducer.PileUpSimulator_E7TeV_Fall2011_Reprocess_inTimeOnly_cff')
+process.load('FastSimulation.Configuration.Geometries_START_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('FastSimulation.Configuration.FamosSequences_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedParameters_cfi')
+process.load('FastSimulation.Configuration.HLT_GRun_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+)
+
+# Input source
+process.source = cms.Source("LHESource",
+    fileNames = cms.untracked.vstring('file:/net/data_cms/institut_3b/tholen/lhef/tt_nodecay/tt_nodecay_0.lhe')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.341.2.2 $'),
+    annotation = cms.untracked.string('MyPackage/TtGammaAnalysis/python/myGenFragmentPythia8noMatch.py nevts:10000'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('file:/netdata_cms/institut_3b/tholen/test/tt_nodecay.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+process.simulation = cms.Sequence(process.simulationWithFamos)
+process.HLTEndSequence = cms.Sequence(process.reconstructionWithFamos)
+process.Realistic7TeV2011CollisionVtxSmearingParameters.type = cms.string("BetaFunc")
+process.famosSimHits.VertexGenerator = process.Realistic7TeV2011CollisionVtxSmearingParameters
+process.famosPileUp.VertexGenerator = process.Realistic7TeV2011CollisionVtxSmearingParameters
+process.GlobalTag.globaltag = 'START44_V10::All'
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen_genonly)
+process.reconstruction = cms.Path(process.reconstructionWithFamos)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.reconstruction,process.RECOSIMoutput_step])
+
