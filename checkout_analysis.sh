5c281b3cc08d9f46ceadcbaf7eec7c06fb3a15cb
Heiner Tholen <heiner.tholen@cern.ch>
2013-10-23 13:20:31 +0200

    adding second chunk

--- checkout_analysis.sh
+++ checkout_analysis.sh
@@ -15,5 +15,17 @@ git cms-merge-topic -u vadler:53X-tagset133511-newBTagging
 git cms-merge-topic -u vadler:53X-tagset133511-newEGIsolation
 git cms-merge-topic -u cms-tau-pog:CMSSW_5_3_X
 
+git remote add -f SCFootprintRemoval https://github.com/peruzzim/SCFootprintRemoval
+git merge -s ours --no-commit SCFootprintRemoval/master
+git read-tree --prefix=PFIsolation/SuperClusterFootprintRemoval -u SCFootprintRemoval/master
+git commit -m 'Merge SCFootprintRemoval in CMSSW'
+git checkout PFIsolation
+
+git remote add -f EGamma-EGammaAnalysisTools https://github.com/ETHZ/EGamma-EGammaAnalysisTools
+git merge -s ours --no-commit V00-00-21 EGamma-EGammaAnalysisTools/master
+git read-tree --prefix=EGamma/EGammaAnalysisTools V00-00-21
+git commit -m 'Merge EGammaAnalysisTools in CMSSW'
+git checkout EGamma
+
 git pull
 
