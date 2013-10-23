a673b6b835b199243ff21272a3d3cb24b1e0a3ee
Heiner Tholen <heiner.tholen@cern.ch>
2013-10-23 13:16:44 +0200

    deleting old analysis

--- TtGammaAnalysis/test/myPhotonSel_cfg.py
+++
@@ -1,7 +0,0 @@
-from MyPackage.TtGammaAnalysis.myPhotonSelection_cfg import *
-
-process.source.fileNames = [
-    'file:/user/tholen/eventFiles/ttgamma_whizard_PatTuple.root',
-]
-
-#    'file:/user/tholen/eventFiles/ttgamma_Enriched_PatTuple.root'
--- TtGammaAnalysis/test/myPhotonSel_noISR_cfg.py
+++
@@ -1,5 +0,0 @@
-
-from MyPackage.TtGammaAnalysis.myPhotonSelection_cfg import *
-
-process.source.fileNames = ['file:/user/tholen/eventFiles/ttgamma_whizard_noISR_PatTuple.root']
-
