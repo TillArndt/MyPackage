7b61cd09a0c9693efb33a5d1dad5bc2184cc3834
Heiner Tholen <heiner.tholen@cern.ch>
2013-10-23 13:14:31 +0200

    adding first set of checkout commands

--- checkout_analysis.sh
+++ checkout_analysis.sh
@@ -1,5 +1,19 @@
 
 # commands to check out full analysis
 
+source /afs/cern.ch/cms/cmsset_default.sh
+cmsrel CMSSW_5_3_11_patch6
+cd CMSSW_5_3_11_patch6/
+git clone https://github.com/heinzK1X/CmsToolsAC3b.git
+ln -s CmsToolsAC3b/cmstoolsac3b python/
+cd src
+cmsenv
+git cms-addpkg PhysicsTools/PatAlgos
+git cms-merge-topic -u vadler:53X-tagset133511
+git cms-addpkg PhysicsTools/PatUtils
+git cms-merge-topic -u vadler:53X-tagset133511-newBTagging
+git cms-merge-topic -u vadler:53X-tagset133511-newEGIsolation
+git cms-merge-topic -u cms-tau-pog:CMSSW_5_3_X
 
+git pull
 
