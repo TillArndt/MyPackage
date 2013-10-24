b26cd274892476e85a9b8e30ec3c0971cac95e01
Heiner Tholen <heiner.tholen@cern.ch>
2013-10-24 14:57:00 +0200

    fixing checkout commands for scfootrm tag and adding theta framework

--- checkout_analysis.sh
+++ checkout_analysis.sh
@@ -15,22 +15,32 @@ git cms-addpkg PhysicsTools/PatUtils
 git cms-merge-topic -u vadler:53X-tagset133511-newBTagging
 git cms-merge-topic -u vadler:53X-tagset133511-newEGIsolation
 git cms-merge-topic -u cms-tau-pog:CMSSW_5_3_X
-
-git remote add -f SCFootprintRemoval https://github.com/peruzzim/SCFootprintRemoval
-git merge -s ours --no-commit SCFootprintRemoval/master
-git read-tree --prefix=PFIsolation/SuperClusterFootprintRemoval -u SCFootprintRemoval/master
-git commit -m 'Merge SCFootprintRemoval in CMSSW'
-git checkout PFIsolation
+scram b -j 9
 
 git remote add -f EGamma-EGammaAnalysisTools https://github.com/ETHZ/EGamma-EGammaAnalysisTools
 git merge -s ours --no-commit EGamma-EGammaAnalysisTools/V00-00-30
 git read-tree --prefix=EGamma/EGammaAnalysisTools -u EGamma-EGammaAnalysisTools/V00-00-30
 git commit -m 'Merge EGammaAnalysisTools in CMSSW'
 git checkout EGamma
+
+git clone https://github.com/peruzzim/SCFootprintRemoval.git
+cd SCFootprintRemoval/
+git checkout V01-01
+cd ../
+mkdir PFIsolation
+mv SCFootprintRemoval PFIsolation/SuperClusterFootprintRemoval
 scram b -j 9
 
 git clone https://github.com/HeinAtCERN/TTGammaModules.git
 git clone https://github.com/HeinAtCERN/MyProducts.git
 git clone https://github.com/HeinAtCERN/MyPackage.git
 scram b -j 9
+
+cd ..
+svn co https://ekptrac.physik.uni-karlsruhe.de/public/theta/tags/testing theta
+cd theta
+make
+cd ../python
+ln -s ../theta/utils2/theta_auto/
+cd ../src
 
