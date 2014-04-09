e657076a089d7ec8ee7dd02780e79848c0c031a0
Till <till.arndt@rwth-aachen.de>
2014-04-09 11:32:22 +0200

    Crabbing changes

--- crabbing/201308WhizPdfTtSel/Madgraph/config.py
+++ crabbing/201308WhizPdfTtSel/Madgraph/config.py
@@ -14,7 +14,7 @@ __builtin__.cms_var = {
 from MyPackage.TtSelection.yv_config import *
 
 process.source.fileNames = [
-    'file:/afs/cern.ch/work/t/tarndt/private/Test/CMSSW_5_3_15/src/MyPackage/crabbing/201308WhizPdfTtSel/Madgraph/out_STEP2_1_1_2e0.root',
+    '',
 ]
 process.out.fileName = 'out_patTuple.root'
 process.TFileService.fileName = 'fileservice.root'
