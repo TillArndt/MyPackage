080d057cde64ed6a1441da2a664072aec4118fb4
Till <till.arndt@rwth-aachen.de>
2014-05-16 11:06:00 +0200

    Temporary Fix for userfloat largest pdgid, can be discarded after next skim

--- TtGamma8TeV/python/cff_preSel.py
+++ TtGamma8TeV/python/cff_preSel.py
@@ -9,8 +9,16 @@ import FWCore.ParameterSet.Config as cms
 
 from MyPackage.TtGamma8TeV.cfi_bTagRequirement import *
 from MyPackage.TtGamma8TeV.cfi_trigger import *
+########################temporary Fix can be deleted after next skim ################
+import cfi_photonUserData as PhotonUser
+
+photonUserDataSCFootRm = PhotonUser.photonUserDataLargestPdgId.clone(
+srcPhoton="photonUserDataSCFootRm"
+)
+########################################
 
 preSel = cms.Sequence(
+    photonUserDataSCFootRm *
     triggerSelection *
     bTagSequence
 )
